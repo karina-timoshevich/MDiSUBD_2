c28b8aa17d0dcd83e031dfbc621b2ffbd111b391
Karina Timoshevich <karina03tima@gmail.com>
2025-01-30 15:10:34 +0300

    update function calculate_annual_reward

--- LR1/CALCULATE ANNUAL REWARD.sql
+++ LR1/CALCULATE ANNUAL REWARD.sql
@@ -12,13 +12,19 @@ BEGIN
     IF p_bonus_percentage < 0 THEN
         RAISE_APPLICATION_ERROR(-20002, 'Процент годовых премиальных не может быть отрицательным.');
     END IF;
+    
+    IF p_bonus_percentage <> TRUNC(p_bonus_percentage) THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Процент годовых премиальных должен быть целым числом.');
+    END IF;
+
     annual_reward := (1 + p_bonus_percentage / 100) * 12 * p_monthly_salary;
     
     RETURN annual_reward;
+
 EXCEPTION
     WHEN OTHERS THEN
         RAISE_APPLICATION_ERROR(-20003, 'Произошла ошибка: ' || SQLERRM);
 END calculate_annual_reward;
 /
-
 SELECT calculate_annual_reward(50000, 20) FROM dual;
+SELECT calculate_annual_reward(50000, 20.5) FROM dual; 
